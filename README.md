38d53e7473ccfd80fa38eea6beb6515590ea550c
mskobe <dou.zo@husky.neu.edu>
2014-12-01 15:40:37 -0500

    source code of experiment

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 cs5610-experiment
 =================
+
+
+This is my experiment for s5610.  
+In this experiment, I focused on the MEAN stack. I created the web services using NodeJS and ExpressJS.  And I also implemented the query, insert, update and delete functions of mongodb.  
+Please go to this [link](http://cs5610-zongkun.rhcloud.com/) to view my experiment.
---
+++ server.js
@@ -0,0 +1,93 @@
+#!/bin/env node
+//  OpenShift sample Node application
+var express = require('express');
+var fs      = require('fs');
+var express = require('express');
+var path = require('path');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Database
+var mongo = require('mongoskin');
+var connection_string = "mongodb://" + "admin" + ":" +
+  "rhZMadL2UQhH" + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  "cs5610";
+var db = mongo.db(connection_string, {native_parser:true});
+
+//var db = mongo.db("mongodb://localhost:27017/nodetest", {native_parser:true});
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080)
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
+//app.set('views', path.join(__dirname, 'views'));
+//app.configure( function() {
+
+//});
+
+//app.get('/', function(req, res){
+//    res.send('Hello World!');
+//});
+
+app.listen(app.get('port'), process.env.OPENSHIFT_NODEJS_IP);
+//app.listen('8080', '127.0.0.1');
